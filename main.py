136ed0e5481e9feaf392c787c485e61203ef430b
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2024-11-29 15:26:33 +0530

    Update Entity add Configuration add pipeline and connect to main

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.datascience import logger
-
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 logger.info('Welcome Custome logger')
+
+
+STAGE_NAME='Data Ingestion Stage'
+
+try:
+  logger.info(f'>> Stage {STAGE_NAME} started >>')
+  obj=DataIngestionTrainingPipeline()
+  obj.initiate_data_ingestion()
+  logger.info(f'>> stage {STAGE_NAME} completed >> \n\nx===x')
+except Exception as e:
+  logger.exception(e)
+  raise e
