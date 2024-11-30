1e269430480715f6f74735e4c0c3538643e88056
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2024-11-30 13:52:04 +0530

    Add model training

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DatavalidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPiprline
+
+from src.datascience.pipeline.model_pipeline import ModelTrainerTrainingPipeline
 logger.info('Welcome Custome logger')
 
 
@@ -36,6 +38,18 @@ try:
   logger.info(f'>> Stage {STAGE_NAME} started >>')
   obj=DataTransformationTrainingPiprline()
   obj.initiate_data_transformation()
+  logger.info(f'>> stage {STAGE_NAME} completed >> \n\nx===x')
+except Exception as e:
+  logger.exception(e)
+  raise e
+
+
+STAGE_NAME='Model trainer stage'
+
+try:
+  logger.info(f'>> Stage {STAGE_NAME} started >>')
+  obj=ModelTrainerTrainingPipeline()
+  obj.initiate_model_training()
   logger.info(f'>> stage {STAGE_NAME} completed >> \n\nx===x')
 except Exception as e:
   logger.exception(e)
